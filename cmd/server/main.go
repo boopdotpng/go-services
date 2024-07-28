2aa2a59fc56cd1cde226ecd8fd5e6ab2de1b4f94
anuraag <anuraag.warudkar@gmail.com>
2024-07-28 15:52:28 -0400

    first go import working

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/boopdotpng/go-services.git/internal/api"
+)
+
 func main() {
 	println("hello, world")
+	api.StartAPI()
 }
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,1 +1,17 @@
-package api 
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func healthcheck(c *fiber.Ctx) error {
+	return c.SendString("hello, world")
+}
+
+func StartAPI() {
+	app := fiber.New()
+
+	app.Get("/healthcheck", healthcheck)
+
+	app.Listen(":3000")
+}
